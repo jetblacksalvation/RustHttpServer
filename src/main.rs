e6eb0308e29f1963c68d15534301a5142e55abc6
coolanimeguy666 <coolanimeguy666@gmail.com>
2024-05-18 00:57:45 -0700

    Fixed client recconnecting

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use local_ip_address::local_ip;
 use std::fs::{self};
-use std::io::prelude::*;
-use std::net::{IpAddr, TcpListener};
+use std::io::{prelude::*, ErrorKind};
+use std::net::{IpAddr, TcpListener,TcpStream};
 use  std::str::*;
 // GET / HTTP/1.1
 // Host: 10.0.0.207:9999
@@ -15,44 +15,21 @@ struct Header {
     key: String,
     value: String,
 }
+fn read_and_handle_errors(stream: &mut TcpStream) {
+    let mut whole_req_bytes = [0; 1024];
 
-fn main() {
-    let my_local_ip: IpAddr = local_ip().unwrap();
-    println!("Hello, server!");
-    println!("http://{my_local_ip}:9999 is my ip!");
+    match stream.read(&mut whole_req_bytes) {
 
-    let listener: TcpListener = std::net::TcpListener::bind(format!("{my_local_ip}:9999")).unwrap();
-    let _ = listener.accept();
-
-    for stream in listener.incoming() {
-        let mut stream = stream.unwrap();
-
-        let mut whole_req_bytes = [0; 1024];
-        loop {
-
-            stream.read(&mut whole_req_bytes).unwrap();            
-
+        Ok(_bytes_read) => {
+            print!("Ok, handling request!");
             let whole_req_str =     from_utf8(&mut whole_req_bytes).unwrap();  
+            println!("{whole_req_str}");
             let line_vec: Vec<_> = whole_req_str.split('\n').collect();
             for line in line_vec {
                 //GET / HTTP/1.0\r\n
                 // println!("is line - {line}");
-                let contents = fs::read_to_string("./hello.html").unwrap();
-
-                if line.contains("GET") {
-                    // / HTTP/1.0\r\n
-                    // let mut stream = TcpStream::connect(format!("{my_local_ip}:9999")).unwrap();
-                    
-                    let response = format!(
-                        "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
-                        contents.len(),
-                        contents
-                    );
-    
-                    stream.write(response.as_bytes()).unwrap();
-                    println!("Line has get!");
-                } 
-                else if line.is_empty()
+                read_the_client_stream(stream, line.clone());
+                if line.is_empty()
                 {
                     println!("Line is empty!");
                 }
@@ -79,7 +56,49 @@ fn main() {
                         println!("Header : key - {key}, val - {val}")
                     }
                 }
+            
             }
         }
+        Err(e) if e.kind() == ErrorKind::ConnectionAborted => {
+            println!("Other side disconnected");
+        }
+        Err(e) => {
+            println!("Some other error occurred: {e}");
+        }
+    }
+    println!("done reading");
+    return;
+
+}
+fn read_the_client_stream(stream: &mut TcpStream, line: &str, ){
+    let contents = fs::read_to_string("./hello.html").unwrap();
+
+    if line.contains("GET") {
+        // / HTTP/1.0\r\n
+        // let mut stream = TcpStream::connect(format!("{my_local_ip}:9999")).unwrap();
+        
+        let response = format!(
+            "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
+            contents.len(),
+            contents
+        );
+
+        stream.write(response.as_bytes()).unwrap();
+        println!("Line has get!");
+    } 
+    else{
+        println!("{line} was not get!!")
+
+    }
+}
+fn main() {
+    let my_local_ip: IpAddr = local_ip().unwrap();
+    println!("Hello, server!");
+    println!("http://{my_local_ip}:9999 is my ip!");
+
+    let listener: TcpListener = std::net::TcpListener::bind(format!("{my_local_ip}:9999")).unwrap();
+    for stream in listener.incoming() {
+        println!("readandhandle");
+        read_and_handle_errors(&mut stream.unwrap())
     }
 }
